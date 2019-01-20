f3e03f2b72c8a6179dfe2626dc4206159107dc90
Aleks Sushitskiy <magnetos.ua@gmail.com>
2019-01-20 16:41:12 +0200

    Create Product Entity

---
+++ src/StoreBundle/Entity/Product.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Symfony\Bundle\SecurityBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="Symfony\Bundle\SecurityBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var \stdClass
+     *
+     * @ORM\Column(name="categoty", type="object")
+     */
+    private $categoty;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="price", type="integer")
+     */
+    private $price;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="images", type="array")
+     */
+    private $images;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set categoty
+     *
+     * @param \stdClass $categoty
+     *
+     * @return Product
+     */
+    public function setCategoty($categoty)
+    {
+        $this->categoty = $categoty;
+
+        return $this;
+    }
+
+    /**
+     * Get categoty
+     *
+     * @return \stdClass
+     */
+    public function getCategoty()
+    {
+        return $this->categoty;
+    }
+
+    /**
+     * Set price
+     *
+     * @param integer $price
+     *
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set images
+     *
+     * @param array $images
+     *
+     * @return Product
+     */
+    public function setImages($images)
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+
+    /**
+     * Get images
+     *
+     * @return array
+     */
+    public function getImages()
+    {
+        return $this->images;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Product
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
+
